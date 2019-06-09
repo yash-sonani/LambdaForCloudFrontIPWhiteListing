b944cc78c6784c31f7c9cf30abb23c47da8cf883
yash-sonani <46063263+yash-sonani@users.noreply.github.com>
2019-06-09 10:43:56 +0530

    Create SG_Operation.py
    This File Update Inboud Rule of SG.

---
+++ SG_Operation.py
@@ -0,0 +1,72 @@
+import boto3
+
+# This Function update security group with given new IP
+
+def update_security_group(client, group, new_ranges, port):
+    added = 0
+    removed = 0
+    
+    if len(group['IpPermissions']) > 0:
+        
+        for permission in group['IpPermissions']:
+            
+            if permission['FromPort'] <= port and permission['ToPort'] >= port :
+                
+                old_prefixes = list()
+                to_revoke = list()
+                to_add = list()
+                for range in permission['IpRanges']:
+                    cidr = range['CidrIp']
+                    old_prefixes.append(cidr)
+                    if new_ranges.count(cidr) == 0:
+                        to_revoke.append(range)
+                        print(group['GroupId'] + ": Revoking " + cidr + ":" + str(permission['ToPort']))
+                
+                for range in new_ranges:
+                    if old_prefixes.count(range) == 0:
+                        to_add.append({ 'CidrIp': range })
+                        print(group['GroupId'] + ": Adding " + range + ":" + str(permission['ToPort']))
+
+                removed += revoke_permissions(client, group, permission, to_revoke)
+                added += add_permissions(client, group, permission, to_add)
+
+    else:
+        to_add = list()
+        for range in new_ranges:
+            to_add.append({ 'CidrIp': range })
+            print(group['GroupId'] + ": Adding " + range + ":" + str(port))
+        permission = { 'ToPort': port, 'FromPort': port, 'IpProtocol': 'tcp'}
+        added += add_permissions(client, group, permission, to_add)
+
+    print (group['GroupId'] + ": Added " + str(added) + ", Revoked " + str(removed))
+    return (added > 0 or removed > 0)
+
+# This Function Remove IP from SG 
+
+def revoke_permissions(client, group, permission, to_revoke):
+    if len(to_revoke) > 0:
+        revoke_params = {
+            'ToPort': permission['ToPort'],
+            'FromPort': permission['FromPort'],
+            'IpRanges': to_revoke,
+            'IpProtocol': permission['IpProtocol']
+        }
+
+        client.revoke_security_group_ingress(GroupId=group['GroupId'], IpPermissions=[revoke_params])
+
+    return len(to_revoke)
+
+# This Function Add IP to SG
+
+def add_permissions(client, group, permission, to_add):
+    if len(to_add) > 0:
+        add_params = {
+            'ToPort': permission['ToPort'],
+            'FromPort': permission['FromPort'],
+            'IpRanges': to_add,
+            'IpProtocol': permission['IpProtocol']
+        }
+
+        client.authorize_security_group_ingress(GroupId=group['GroupId'], IpPermissions=[add_params])
+
+    return len(to_add)
